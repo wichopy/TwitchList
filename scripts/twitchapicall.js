c9976b210d6296fb053e41df7dce50f3536f140d
william chou <w.chou06@gmail.com>
2017-02-13 08:23:14 -0500

    Update status and retreive stream data

--- scripts/twitchapicall.js
+++ scripts/twitchapicall.js
@@ -1,6 +1,6 @@
-var channels = ["playhearthstone","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var channels = ["playhearthstone", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 //var JSON = require('JSON');
-function addrow(chan,chanpicurl) {
+function addrow(chan, chanpicurl) {
   var newdiv = document.createElement('div');
   var table = document.getElementById('streams-table');
   //console.log(table);
@@ -9,29 +9,66 @@ function addrow(chan,chanpicurl) {
   newdiv.className = "row"; //can't use class its a reserved word, use className to update class.
   table.appendChild(newdiv);
   console.log("4. Create div and populate with channel name and logo.")
-  newdiv.innerHTML = `<img src="${chanpicurl}" style="width:100px;height:100px;" \> <b> ${chan} </b>`;
+  newdiv.innerHTML = `<div class="col-md-4"><img src="${chanpicurl}" style="width:100px;height:100px;" \></div>
+                      <div class="col-md-6"><p><b> ${chan} </b></p></div>
+                      <div class="col-md-2"><p id="${chan}-status">Status: Retreiving...</p></div>`;
+}
+
+function updatestatus(chan, newmsg) {
+  var status = document.getElementById(chan + '-status');
+  console.log(chan + '-status');
+  if (newmsg === "Status: Live") {
+    status.innerHTML = `${newmsg}`;
+  } else {
+    status.innerHTML = newmsg;
+  }
 }
 
 function grabuserdata(channel) {
-  var userdata = $.getJSON('https://wind-bow.gomix.me/twitch-api/users/'+channel, function(data) {
-    //console.log(data);
-  })
-    .done(function() {
+  var userdata = $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + channel, function (data) {
+      //console.log(data);
+    })
+    .done(function () {
       console.log("2. Finished grabbing data, console log retrieved data:");
       //console.log(userdata);
       console.log("3. Now adding row of stream.")
-      console.log(userdata.responseText);//Had to parse the responseText field!!
+      console.log(userdata.responseText); //Had to parse the responseText field!!
       var JSONObj = JSON.parse(userdata.responseText);
       console.log(JSONObj);
       addrow(channel, JSONObj.logo);
     });
+  userdata.done(function () {
+    var streamdata = $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channel, function (data) {
+      //console.log(data);
+    }).done(function () {
+      //console.log(streamdata);
+      console.log("done getting stream data");
+      console.log(streamdata.responseText);
+      var StreamJSON = JSON.parse(streamdata.responseText);
+      if (StreamJSON['stream'] === null) {
+        updatestatus(channel, "Status: Offline");
+      } else {
+        console.log(StreamJSON.stream.preview.large);
+        console.log(StreamJSON.stream.channel.url);
+        updatestatus(channel, "Status: Live");
+      }
+    });
+  });
+  userdata.done(function () {
+    var channeldata = $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + channel, function (data) {
+      //   console.log(data);
+    }).done(function () {
+      console.log(channeldata.responseText);
+    });
+    //});
+  });
 
 
 }
-$(document).ready(function() {
+$(document).ready(function () {
 
   //populate channel list.
-  channels.forEach( function (channel) {   
+  channels.forEach(function (channel) {
     // var streamdata = $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+channel, function(data) {
     //   console.log(data);
     // });
@@ -43,4 +80,3 @@ $(document).ready(function() {
     grabuserdata(channel); // CALLBACKS! printing Rows before pictures finish downloading. Need to have the picture download function run first THEN run the write row function.
   });
 });
-
