baea328f73b1247f742906d5655bd760f52e2ab3
William Chou <w.chou06@gmail.com>
2017-02-18 21:19:58 +0000

    begin implementing search bar

--- scripts/twitchapicall.js
+++ scripts/twitchapicall.js
@@ -8,9 +8,9 @@ function addrow(chan, chanpicurl) {
   newdiv.id = chan;
   newdiv.className = "row"; //can't use class its a reserved word, use className to update class.
   table.appendChild(newdiv);
-  console.log("4. Create div and populate with channel name and logo.")
+  //console.log("4. Create div and populate with channel name and logo.")
   newdiv.innerHTML = `<div class="col-md-4"><img src="${chanpicurl}" style="width:100px;height:100px;" \></div>
-                      <div class="col-md-6"><p><b> ${chan} </b></p><p id="${chan}-info-here"> Stream Info Here
+                      <div class="col-md-6"><span id='${chan}-desc'><b> ${chan} </b></span><p id="${chan}-info-here"> Stream Info Here
                       </p></div>
                       <div class="col-md-2"><p id="${chan}-status">Status: Retreiving...</p>
                       </div>`;
@@ -18,7 +18,7 @@ function addrow(chan, chanpicurl) {
 
 function updatestatus(chan, newmsg) {
   var status = document.getElementById(chan + '-status');
-  console.log(chan + '-status');
+  //console.log(chan + '-status');
   if (newmsg === "Status: Live") {
     status.innerHTML = `${newmsg}`;
   } else {
@@ -26,10 +26,12 @@ function updatestatus(chan, newmsg) {
   }
 }
 
-function addStreamInfo(chan, chanprev, chanurl) {
+function addStreamInfo(chan, chanprev, chanurl, streaminfo) {
   var streaminfo = document.getElementById(chan + "-info-here");
-  console.log(`this is my stream info dom object ${streaminfo}`);
-  streaminfo.innerHTML = `<a href="${chanurl}"><img src="${chanprev}" ></a>`;
+  //console.log(`this is my stream info dom object ${streaminfo}`);
+  streaminfo.innerHTML = `<a href="${chanurl}"><img src="${chanprev}" ></a> Description: <span>${streaminfo}</span> `;
+  var StreamTitle = document.getElementById(chan + "-desc");
+  StreamTitle.innerHTML = `${chan} - ${streaminfo}`;
 }
 
 function grabuserdata(channel) {
@@ -37,12 +39,12 @@ function grabuserdata(channel) {
       //console.log(data);
     })
     .done(function () {
-      console.log("2. Finished grabbing data, console log retrieved data:");
+      //console.log("2. Finished grabbing data, console log retrieved data:");
       //console.log(userdata);
-      console.log("3. Now adding row of stream.")
-      console.log(userdata.responseText); //Had to parse the responseText field!!
+      //console.log("3. Now adding row of stream.")
+      //console.log(userdata.responseText); //Had to parse the responseText field!!
       var JSONObj = JSON.parse(userdata.responseText);
-      console.log(JSONObj);
+      //console.log(JSONObj);
       addrow(channel, JSONObj.logo);
     });
   userdata.done(function () {
@@ -50,16 +52,16 @@ function grabuserdata(channel) {
       //console.log(data);
     }).done(function () {
       //console.log(streamdata);
-      console.log("done getting stream data");
-      console.log(streamdata.responseText);
+      //console.log("done getting stream data");
+      //console.log(streamdata.responseText);
       var StreamJSON = JSON.parse(streamdata.responseText);
       if (StreamJSON['stream'] === null) {
         updatestatus(channel, "Status: Offline");
       } else {
-        console.log(StreamJSON.stream.preview.large);
         console.log(StreamJSON.stream.channel.url);
+        console.log(StreamJSON.stream.channel.status);
         updatestatus(channel, "Status: Live");
-        addStreamInfo(channel, StreamJSON.stream.preview.large, StreamJSON.stream.channel.url);
+        addStreamInfo(channel, StreamJSON.stream.preview.large, StreamJSON.stream.channel.url, StreamJSON.stream.channel.status);
       }
     });
   });
@@ -67,7 +69,7 @@ function grabuserdata(channel) {
     var channeldata = $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + channel, function (data) {
       //   console.log(data);
     }).done(function () {
-      console.log(channeldata.responseText);
+      //console.log(channeldata.responseText);
     });
     //});
   });
@@ -85,7 +87,7 @@ $(document).ready(function () {
     //   console.log(data);
     // });
     //});
-    console.log("1. Loop through each channel id and run a request to twitch API");
+    //console.log("1. Loop through each channel id and run a request to twitch API");
     grabuserdata(channel); // CALLBACKS! printing Rows before pictures finish downloading. Need to have the picture download function run first THEN run the write row function.
   });
 });
